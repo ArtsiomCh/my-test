cebcd15fc464549bddd45313d04115827fe1fe05
Eugene Kudelevsky <Eugene.Kudelevsky@jetbrains.com>
2014-08-29 14:09:58 +0400

    commit_13

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World2115!");
+        System.out.println("Hello World2116!");
 
 
         System.out.println("Hello World2114!");
