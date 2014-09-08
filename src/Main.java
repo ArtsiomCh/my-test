250f9abe6f4b01fdaf73f720948f12a7194f17ba
Eugene Kudelevsky <Eugene.Kudelevsky@jetbrains.com>
2014-09-08 16:06:29 +0400

    int 3

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World2116!");
-
-
-        System.out.println("Hello World2114!");
-        System.out.println("Hello World2114!");
-
-        System.out.println("Hello World2114111!");
-        System.out.println("Hello World2116!");
-
-        System.out.println("Hello World2114!");
-        System.out.println("Hello World2114!");
-
-        System.out.println("Hello World2116!");
-        System.out.println("Hello World2116!");
-
-        System.out.println("Hello World2117!");
-        System.out.println("Hello World2117!");
-        System.out.println("Hello World2117!");
+        int a = 12;
     }
 }
