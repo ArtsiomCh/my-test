7055e93c193938a736bbdbf99fb0b60b37d3b88c
Eugene Kudelevsky <Eugene.Kudelevsky@jetbrains.com>
2014-10-21 19:56:59 +0400

    commit16

---
+++ src/NewFile5.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Eugene.Kudelevsky on 21.10.2014.
+ */
+public class NewFile5 {
+    public void f() {
+    }
+
+    public void g() {
+    }
+
+
+
+
+
+
+
+    public void h() {
+    }
+}
