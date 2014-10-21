61e6972e99731d3e29d7c16c2a237c24b1a80d21
Eugene Kudelevsky <Eugene.Kudelevsky@jetbrains.com>
2014-10-21 16:26:16 +0400

    commit14

---
+++ src/NewFile3.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Eugene.Kudelevsky on 21.10.2014.
+ */
+public class NewFile3 {
+
+    public void f() {
+    }
+
+    public void g() {
+    }
+
+
+
+
+
+
+
+    public void h() {
+    }
+}
