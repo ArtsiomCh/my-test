2d25d666d25823c1d9d4d2ef10cc0d9f4860f3ce
Eugene Kudelevsky <Eugene.Kudelevsky@jetbrains.com>
2014-10-21 20:07:42 +0400

    commit19

---
+++ src/NewFile7.java
@@ -0,0 +1,28 @@
+/**
+ * Created by Eugene.Kudelevsky on 21.10.2014.
+ */
+public class NewFile7 {
+    public void f() {
+    }
+
+    public void g() {
+    }
+
+
+
+
+
+
+
+    public void h() {
+    }
+
+    public void pu() {
+    }
+
+    public void pu1() {
+    }
+
+    public void pu2() {
+    }
+}
