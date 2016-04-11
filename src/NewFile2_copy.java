755d4ca760f488a1ff00056f368f034afcade4df
Eugene Kudelevsky <Eugene.Kudelevsky@jetbrains.com>
2016-04-11 19:37:25 +0300

    copy

---
+++ src/NewFile2_copy.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Eugene.Kudelevsky on 21.10.2014.
+ */
+public class NewFile2_copy {
+    public void f() {
+    }
+    public void h(String param) {
+        if (param == null) {
+            System.out.println("hi");
+            System.out.println("hi");
+            System.out.println("hi");
+        }
+        System.out.println(param.length());
+        int a = 1;
+        a += 2;
+        new NewFile3().f();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
