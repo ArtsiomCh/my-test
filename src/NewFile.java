2836c9f82531351f8ff9cb5c599b1892c24a6115
Eugene Kudelevsky <Eugene.Kudelevsky@jetbrains.com>
2016-05-19 16:55:16 +0300

    yyy

--- src/NewFile.java
+++ src/NewFile.java
@@ -2,7 +2,7 @@
  * Created by Eugene.Kudelevsky on 10.10.2014.
  */
 public class NewFile {
-    public static void main(String[] args)  {
+    public static void main(String[] arg)  {
          // 1234568
     }
 }   
